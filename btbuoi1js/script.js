9531a810bfbae0a75d0a90e762bce83cc0ebcd83
tavamhieu <125560675+tavamhieu@users.noreply.github.com>
2024-04-15 13:38:26 +0700

    update bài js

---
+++ btbuoi1js/script.js
@@ -0,0 +1,39 @@
+// function calculateFactorial(number) {
+//     if (number === 0 || number === 1) {
+//         return 1; 
+//     } else {
+//         let factorial = 1;
+//         for (let i = 2; i <= number; i++) {
+//             factorial *= i;
+//         }
+//         return factorial;
+//     }
+// }
+
+// let soDuong = 5;
+// let ketQua = calculateFactorial(soDuong);
+// console.log("Giai thừa của", soDuong, "=", ketQua);
+
+
+2
+// function reverseString(str) {
+//     let reversed = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversed += str[i];
+//     }
+//     return reversed;
+// }
+// let chuoi = "hello";
+// let ketQua = reverseString(chuoi);
+// console.log("Chuỗi đảo ngược:", ketQua);
+4
+// function subString(str) {
+//     if (str.length > 15) {
+//         return str.slice(0, 10) + "...";
+//     } else {
+//         return str;
+//     }
+// }
+// let chuoi = "xinchaocacbandenvoiTechmaster";
+// let ketQua = subString(chuoi);
+// console.log("Kết quả:", ketQua);
